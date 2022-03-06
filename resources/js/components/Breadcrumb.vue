e3a540050281439118bdbd4ae959a52b74424ac8
Germain Carré <git@germain.sh>
2022-03-06 06:23:42 +0100

    Mobile-friendly breadcrumb

--- resources/js/components/Breadcrumb.vue
+++ resources/js/components/Breadcrumb.vue
@@ -1,20 +1,21 @@
 <template>
     <nav class="flex" aria-label="Breadcrumb">
-        <ol role="list" class="bg-white dark:bg-gray-700 rounded-md shadow px-6 flex space-x-4">
-            <li class="flex">
+        <ol role="list" class="bg-white dark:bg-gray-700 rounded-md shadow px-6 py-2 lg:py-0 grid grid-cols-1 lg:flex space-4">
+            <li class="flex my-1">
                 <div class="flex items-center">
                     <a :href="homeRoute" class="text-gray-400 hover:text-gray-500 dark:text-white dark:hover:text-gray-400">
                         <i class="fas fa-home mr-1"></i>
-                        <span class="sr-only">Home</span>
+                        <span class="lg:hidden">Home</span>
                     </a>
                 </div>
             </li>
-            <li v-for="page in pages" :key="page.name" class="flex">
+            <li v-for="page in pages" :key="page.name" class="flex my-1">
                 <div class="flex items-center">
-                    <svg class="flex-shrink-0 w-6 h-full text-gray-200 dark:text-gray-500" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <i class="mx-1 lg:hidden fas fa-chevron-right flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <svg class="hidden lg:block flex-shrink-0 w-6 h-full text-gray-200 dark:text-gray-500" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
                     </svg>
-                    <a :href="page.href" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-200 dark:hover:text-gray-400" :aria-current="page.current ? 'page' : undefined">{{ page.name }}</a>
+                    <a :href="page.href" class="lg:ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-200 dark:hover:text-gray-400" :aria-current="page.current ? 'page' : undefined">{{ page.name }}</a>
                 </div>
             </li>
         </ol>
